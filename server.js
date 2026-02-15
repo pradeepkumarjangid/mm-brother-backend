6239b234eedd79b25eeb2988929c6ffc7ef3cdaf
pradeepkumarjangid <pkjangid0025@gmail.com>
2026-02-15 13:27:24 +0530

    update cors

--- server.js
+++ server.js
@@ -9,12 +9,7 @@ connectDB();
 const app = express();
 
 // app.use(cors());
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json()); 
 app.use("/uploads", express.static("uploads"));
 
